22f56c09c208ec4eca7e22de24cda682d8a5a06e
topus009 <sj_89@mail.ru>
2018-01-22 08:51:05 +0300

    03 - вынес апи в отдельный файл

--- App.js
+++ App.js
@@ -1,33 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View, FlatList, ScrollView, Image } from 'react-native';
+import getEvents from './src/getEvents';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
       this.state = {
         events: [],
+        current_page: 1,
       }
     }
-
-
+  
   componentDidMount() {
-    const q = 'event';
-    const location = 'spb';
-    const page_size = 10;
-    const expand = 'images,place,location,dates,participants';
-    const fields = 'body_text,title,dates,id,place,images';
-    const page = 1;
-    const text_format = 'plain';
-    
-    const baseApi = 'https://kudago.com/public-api/v1.3/';
-    const url = `events/?location=${location}&page_size=${page_size}&expand=${expand}&fields=${fields}&page=${page}&text_format=${text_format}`;
-
-    let arr = [];
-    fetch(baseApi + url).then(res => {
-        this.setState({ events: JSON.parse(res._bodyText).results });
-        arr.push(JSON.parse(res._bodyText).results);
-    });
-    console.warn(arr);
+    const { current_page } = this.state;
+    getEvents(current_page).then(res => {
+          this.setState({ events: JSON.parse(res._bodyText).results });
+      });
   }
 
   render() {
